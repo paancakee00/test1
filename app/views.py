727bd97df1f53d0821dd1b5bac1bd0facff47d13
paancakee00 <rubionnoriel2@gmail.com>
2024-11-14 06:43:55 +0800

    commit

---
+++ app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = 'app/home.html'
+
+class AboutPageView(TemplateView):
+    template_name = 'app/about.html'
+
+class BasePageView(TemplateView):
+    template_name = 'app/base.html'
